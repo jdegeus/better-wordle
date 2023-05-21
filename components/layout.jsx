8279fc3d4ad51c60a369c44fc80ff193e7e945a3
Jonathan <jonathandegeus@hotmail.nl>
2023-05-22 00:19:49 +0200

    changed font and color

---
+++ components/layout.jsx
@@ -0,0 +1,21 @@
+// import Navbar from './navbar';
+// import Footer from './footer';
+
+import { Ubuntu } from 'next/font/google';
+
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ["300", "400", "500", "700"]
+});
+
+ 
+export default function Layout({ children }) {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <main className={ubuntu.className}>{children}</main>
+      {/* <Footer /> */}
+    </>
+  );
+}
